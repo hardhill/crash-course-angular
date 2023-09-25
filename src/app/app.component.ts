8484dc923cb8179fa27edc116c0df505f4ad108b
Waclaw Bielecki <hardhill@mail.ru>
2023-09-25 22:32:49 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {IProduct} from "./models/product";
+import {ProductsService} from "./services/products.service";
+import {Observable, tap} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'crash-course';
+export class AppComponent implements OnInit{
+  title = 'application course';
+  products$:Observable<IProduct[]>
+  loading = false
+  textSearch: string = '';
+
+  constructor(private productsService:ProductsService) {
+  }
+  ngOnInit(): void {
+    this.loading = true
+    this.products$ = this.productsService.getAll().pipe(
+      tap(()=>{
+        this.loading = false
+      })
+    )
+
+  }
 }
